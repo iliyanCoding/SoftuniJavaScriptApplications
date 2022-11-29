96409f352f32ebcc1c2f97a0f770e8d9bae47834
Iliyan Muradilov <102465153+iliyanCoding@users.noreply.github.com>
2022-11-29 09:00:59 +0200

    Create api.js

---
+++ ExamPrep/src/api/api.js
@@ -0,0 +1,47 @@
+import { getUserData } from "../util.js";
+
+const host = 'http://localhost:3030/'
+
+async function request(method, url, data){
+    const options = {
+        method,
+        headers: {}
+    }
+
+    if(data !== undefined){
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const user = getUserData();
+
+    if(user){
+        options.headers['X-Authorization'] = user.accessToken;
+    }
+
+    try{
+
+        const repsponse = await fetch(host + url, options);
+
+        if (repsponse.status == 204){
+            return Response;
+        }
+
+        const result = await repsponse.json();
+
+        if(repsponse.ok == false){
+            throw new Error(result.message);
+        }
+
+        return result;
+
+    }catch(err){
+        alert(err.message);
+        throw err;
+    }
+}
+
+export const get = request.bind(null, 'get');
+export const post = request.bind(null, 'post');
+export const put = request.bind(null, 'put');
+export const del = request.bind(null, 'delete');
