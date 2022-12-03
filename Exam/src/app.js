4f2da9647bb04be2e93ba0c9427bc6c9563f4623
Iliyan Muradilov <102465153+iliyanCoding@users.noreply.github.com>
2022-12-03 08:41:28 +0200

    Create app.js

---
+++ Exam/src/app.js
@@ -0,0 +1,46 @@
+import { render, page } from "./lib.js";
+import { getUserData } from "./util.js";
+import { showCatalog } from "./views/catalog.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
+import { showEdit } from "./views/edit.js";
+import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { updateNav } from "./views/nav.js";
+import { showRegister } from "./views/register.js";
+
+const main = document.querySelector('main');
+
+//document.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+page('/', showHome);
+page('/login', showLogin);
+page('/register', showRegister);
+page('/catalog', showCatalog);
+page('/catalog/:id', showDetails);
+page('/edit/:id', showEdit)
+page('/create', showCreate);
+
+updateNav();
+page.start();
+
+function decorateContext(ctx, next){
+    ctx.render = renderMain;
+    ctx.updateNav = updateNav;
+
+    const user = getUserData();
+    if(user){
+        ctx.user = user;
+    }
+    
+    next();
+}
+
+function renderMain(content){
+    render(content, main);
+}
+
+
+
+
